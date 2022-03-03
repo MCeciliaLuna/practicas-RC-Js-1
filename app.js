58d27739425b19748cd8a0853741d0e5d83f6c12
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-03-03 11:36:04 -0300

    Subo repositorio completo de primeras prácticas de Js

---
+++ app.js
@@ -0,0 +1,32 @@
+const num1 = 10
+const num2 = 20
+let = (num1 + num2)
+console.log(let);
+
+const num3 = 30
+const num4 = 40
+console.log(num3+num4);
+
+const num5 = 50
+const num6 = 60
+res = num5 + num6;
+
+const text = ('Un texto cualquiera ');
+
+let = (text + num4)
+console.log(let);
+
+console.log(res+num4);
+
+console.log('Sólo texto, ¿para qué');
+
+if (num1 == 10){
+  console.log('es el mismo número')
+}
+
+if (num1 == 20){
+  console.log('no es el mismo')
+}
+
+if (let == 'Un texto cualquiera ' + num4)
+  console.log('Dice lo mismo')
